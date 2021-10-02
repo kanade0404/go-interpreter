70acbb5ecc7aa89b58239134013389a09bc3f693
kanade <melty0404@gmail.com>
2021-10-02 21:15:49 +0900

    chapter 3.5

--- evaluator/evaluator.go
+++ evaluator/evaluator.go
@@ -31,9 +31,15 @@ func Eval(node ast.Node) object.Object {
 	// 真偽値
 	case *ast.Boolean:
 		return nativeBoolToBooleanObject(node.Value)
+	// 前置演算子
 	case *ast.PrefixExpression:
 		right := Eval(node.Right)
 		return evalPrefixExpression(node.Operator, right)
+	// 中置演算子
+	case *ast.InfixExpression:
+		left := Eval(node.Left)
+		right := Eval(node.Right)
+		return evalInfixExpression(node.Operator, left, right)
 	}
 	return nil
 }
@@ -99,4 +105,51 @@ func evalMinusPrefixOperatorExpression(right object.Object) object.Object {
 	}
 	value := right.(*object.Integer).Value
 	return &object.Integer{Value: -value}
+}
+
+/*
+evalInfixExpression
+中置演算子式の評価
+オペランドを先に評価して両方整数でないならNULLを返す
+ */
+func evalInfixExpression(operator string, left, right object.Object) object.Object {
+	switch  {
+	case left.Type() == object.INTEGER_OBJ && right.Type() == object.INTEGER_OBJ:
+		return evalIntegerInfixExpression(operator, left, right)
+	case operator == "==":
+		return nativeBoolToBooleanObject(left == right)
+	case operator == "!=":
+		return nativeBoolToBooleanObject(left != right)
+	default:
+		return NULL
+	}
+}
+
+/*
+evalIntegerInfixExpression
+オペランドの両方が整数である中置演算子式の評価
+ */
+func evalIntegerInfixExpression(operator string, left, right object.Object) object.Object {
+	leftVal := left.(*object.Integer).Value
+	rightVal := right.(*object.Integer).Value
+	switch operator {
+	case "+":
+		return &object.Integer{Value: leftVal + rightVal}
+	case "-":
+		return &object.Integer{Value: leftVal - rightVal}
+	case "*":
+		return &object.Integer{Value: leftVal * rightVal}
+	case "/":
+		return &object.Integer{Value: leftVal / rightVal}
+	case "<":
+		return nativeBoolToBooleanObject(leftVal < rightVal)
+	case ">":
+		return nativeBoolToBooleanObject(leftVal > rightVal)
+	case "==":
+		return nativeBoolToBooleanObject(leftVal == rightVal)
+	case "!=":
+		return nativeBoolToBooleanObject(leftVal != rightVal)
+	default:
+		return NULL
+	}
 }
